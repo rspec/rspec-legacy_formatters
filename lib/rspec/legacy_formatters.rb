79d00071ab7c15fefcb44a109c68db6e633290f1
Jon Rowe <hello@jonrowe.co.uk>
2014-04-13 13:39:12 +1000

    namespace docs

--- lib/rspec/legacy_formatters.rb
+++ lib/rspec/legacy_formatters.rb
@@ -1,6 +1,8 @@
 require "rspec/legacy_formatters/version"
 
+# Namespace for the rspec code.
 module RSpec
+  # Namespace for the rspec-legacy_formatters code.
   module LegacyFormatters
   end
 end
