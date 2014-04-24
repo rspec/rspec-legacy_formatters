2f6d635f83c4ca65b65df5f9922956915efc3fe1
Myron Marston <myron.marston@gmail.com>
2014-04-24 08:14:57 -0700

    Load rspec/legacy_formatters early

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require 'rspec/support/spec'
 RSpec::Support::Spec.setup_simplecov
 
+# This MUST be loaded early, before rspec-core's formatters are loaded.
+require 'rspec/legacy_formatters'
+
 Dir['./spec/support/**/*.rb'].map { |f| require f }
