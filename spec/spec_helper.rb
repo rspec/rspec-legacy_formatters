5292f637918f51877d7f822fdbdbf74ec922df7c
Jon Rowe <hello@jonrowe.co.uk>
2014-10-06 21:51:54 +1100

    fixup legacy formatters on 1.9.2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'rspec/support/spec'
 RSpec::Support::Spec.setup_simplecov
 
+require 'rspec/core/formatters/console_codes'
+
 # This MUST be loaded early, before rspec-core's formatters are loaded.
 require 'rspec/legacy_formatters'
 
